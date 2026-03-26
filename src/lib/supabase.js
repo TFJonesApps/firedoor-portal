0c26af210e7a7c0b838b5c2209535e761bfcbb50
TFJonesApps <apps@tfjones.com>
2026-03-26 11:21:13 +0000

    Hardcode Supabase credentials for static build

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(
+  'https://ztagewwelwgrhmibikcv.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0YWdld3dlbHdncmhtaWJpa2N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0NTMzNDQsImV4cCI6MjA5MDAyOTM0NH0.cC8_Ltldb4fRB9nHNCNCZIN2N7R_cD1WIFP6oMVyrG8'
+)
