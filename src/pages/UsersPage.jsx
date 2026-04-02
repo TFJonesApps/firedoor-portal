6384d608646d87c7b8648213ab8b552c6ea5097b
TFJonesApps <apps@tfjones.com>
2026-04-02 10:06:37 +0100

    Update UsersPage.jsx

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -45,6 +45,7 @@ export default function UsersPage() {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${session.access_token}`,
+        'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0YWdld3dlbHdncmhtaWJpa2N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0NTMzNDQsImV4cCI6MjA5MDAyOTM0NH0.cC8_Ltldb4fRB9nHNCNCZIN2N7R_cD1WIFP6oMVyrG8',
       },
       body: JSON.stringify(body),
     })
